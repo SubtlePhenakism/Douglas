516c926941e758aa821a1768fb769a644a14d18a
SubtlePhenakism <subtlephenakism@gmail.com>
2015-09-11 12:41:29 -0700

    Excalibur Login and Menu

---
+++ Excalibur/HomeViewController.swift
@@ -0,0 +1,64 @@
+//
+//  HomeViewController.swift
+//  Excalibur
+//
+//  Created by Robert Passemar on 9/10/15.
+//  Copyright (c) 2015 TreeNine. All rights reserved.
+//
+
+import UIKit
+import Parse
+import Bolts
+
+class HomeViewController: UIViewController {
+    
+    @IBAction func logOutAction(sender: AnyObject){
+        
+        // Send a request to log out a user
+        PFUser.logOut()
+        
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Login") as! UIViewController
+            self.presentViewController(viewController, animated: true, completion: nil)
+        })
+        
+    }
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Show the current visitor's username
+        if let pUserName = PFUser.currentUser()?["username"] as? String {
+            self.userNameLabel.text = "@" + pUserName
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        if (PFUser.currentUser() == nil) {
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                
+                let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Login") as! UIViewController
+                self.presentViewController(viewController, animated: true, completion: nil)
+            })
+        }
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
---
+++ Excalibur/ResetPasswordViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ResetPasswordViewController.swift
+//  Excalibur
+//
+//  Created by Robert Passemar on 9/10/15.
+//  Copyright (c) 2015 TreeNine. All rights reserved.
+//
+
+import UIKit
+import Parse
+import Bolts
+
+class ResetPasswordViewController: UIViewController {
+    
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBAction func passwordReset(sender: AnyObject) {
+        var email = self.emailField.text
+        var finalEmail = email.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        
+        // Send a request to reset a password
+        PFUser.requestPasswordResetForEmailInBackground(finalEmail)
+        
+        var alert = UIAlertController (title: "Password Reset", message: "An email containing information on how to reset your password has been sent to " + finalEmail + ".", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
